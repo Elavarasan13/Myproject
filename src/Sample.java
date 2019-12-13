c64ad2889761bee202ebaa73ddc3cc8ab1935b6a
Elavarasan13 <elavarasandevaraj@gmail.com>
2019-12-13 16:47:19 +0530

    frst conflict

--- src/Sample.java
+++ src/Sample.java
@@ -4,7 +4,9 @@ public class Sample {
 	public void empName() {
 		System.out.println("emp name is surya");
 	}
-	
+	public void empPhn() {
+		System.out.println("emp phn num is 958794563");
+	}
 	
 	
 	
